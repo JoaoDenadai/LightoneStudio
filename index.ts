8ab565666d393e0f403d3aedfe44a4a36312cf82
João Denadai <denadai.personal@gmail.com>
2025-04-11 14:22:27 -0300

    Main

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import { Logs } from './source/libraries/modules.js';
-import { createFileOnDisk_Sync } from './source/modules/Files.js';
 
 (async function Main(){
-    createFileOnDisk_Sync({Name: 'Joao', Age: 20}, 'Hello.json');
+    
 })();
